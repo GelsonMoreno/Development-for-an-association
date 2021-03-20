68d4b7820ac4ae380fab1ea002481e385cd069cf
Gelson Moreno <a39224@alunos.ipb.pt>
2021-03-20 17:27:46 +0000

    initial commit

---
+++ views/layouts/main.php
@@ -0,0 +1,29 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title>Hello World!</title>
+</head>
+<body>
+    Hello World!
+</body>
+</html>
+<?php $this->endPage() ?>
