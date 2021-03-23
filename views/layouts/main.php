f43f59bdcc106e53798ed15ebccf91b03ee16b95
Gelson Moreno <a39224@alunos.ipb.pt>
2021-03-23 16:33:38 +0000

    Second commit
    Não sei porque mas sim.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -20,7 +20,7 @@ AppAsset::register($this);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
-    <title>Hello World!</title>
+    <title>Transtec</title>
     <?php $this->head() ?>
 </head>
 <?php $this->beginBody() ?>
@@ -43,8 +43,10 @@ AppAsset::register($this);
         </div>
     </div>
 
+
+
 </header>
-<div id="content">
+<div class="main-content">
   <?= $content ?>
 </div>
 <footer>
--- views/site/index.php
+++ views/site/index.php
@@ -1,1 +1,32 @@
-Hello Sky!
+
+<div class='row'>
+    <div class='column'>
+        <div class='blue-column'>
+           <div class="wall_title_blue">Contact Us</div>
+            <form action="">
+                <div class="form-group">
+                    <label for="name">Nome da Empresa</label>
+                    <input type="name" class="form-control" placeholder="Nome" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" placeholder="Email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="number">Número do telefone</label>
+                    <input type="tel" class="form-control" placeholder="Número" id="number">
+                </div>
+                <div class="form-group">
+                    <label for="description">Descrição</label>
+                    <input type="text" class="form-control input-lg" placeholder="Descrição" id="description">
+                </div>
+                <button type="submit" class="btn btn-default">Enviar</button>
+            </form>
+        </div>
+    </div>
+    <div class='column'>
+        <div class='green-column'>
+           <div class="wall_title_green">Notícias</div>
+        </div>
+    </div>
+</div>
