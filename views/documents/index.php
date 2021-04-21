c9425dba67072122c826ccd4d1f32bbedf2686ae
Gelson Moreno <a39224@alunos.ipb.pt>
2021-04-21 18:22:42 +0100

    mistake XD

--- views/documents/index.php
+++ views/documents/index.php
@@ -7,10 +7,33 @@ use yii\helpers\Html; ?>
     </div>
     <div class="col-lg">
       <?= $this->render('../shared/content_header', ['new_url'=>'index.php?r=documents/new']);?>
-        <div id="document_list">
-          <?= $this->render('_index', ['documents' => $documents]);?>
-        </div>
+        <table id="t01">
+            <tr>
+                <th>Título</th>
+                <th>Descrição</th>
+                <th>Data</th>
+                <th>Tipo</th>
+                <th></th>
+            </tr>
+          <?php foreach ($documents as $document_data): ?>
+              <tr>
+                  <td><?= $document_data->title ?></td>
+                  <td> <?= yii\helpers\BaseStringHelper::truncate($document_data->description, 40) ?></td>
+                  <td><?= $document_data->date ?></td>
+                  <td><?= $document_data->type ?></td>
+                  <td class="td_button">
+                      <a href="/index.php?r=documents/show&document_id=<?= $document_data->id ?>"><i class="bi bi-eye-fill"></i></a>
+                      <a href="<?= $document_data->documentUrlForDownload() . '&inline=true' ?>" target="_blank"><i class="bi bi-file-earmark-text-fill"></i></a>
+                      <a href="/index.php?r=documents/update&document_id=<?= $document_data->id ?>">
+                          <i class="bi bi-pen-fill"></i>
+                      </a>
+                      <a href="/index.php?r=documents/delete&document_id=<?= $document_data->id ?>" data-confirm="Are you sure?">
+                          <i class="bi bi-trash-fill"></i>
+                      </a>
+                  </td>
+              </tr>
+          <?php endforeach;?>
+        </table>
     </div>
-
 </div>
 
