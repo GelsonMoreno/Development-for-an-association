8513769036080e7124e99df4024b1d44f86cb9d3
Gelson Moreno <a39224@alunos.ipb.pt>
2021-04-25 19:57:30 +0100

    ficheiro requerid

--- views/documents/form.php
+++ views/documents/form.php
@@ -23,19 +23,27 @@ use \app\models\User_types;
   const input_type_field = document.querySelector('#documents-type');
 
   input_field.onchange = function(){
+      $("#error_file_input").remove();
       if(!this.value) return;
       const file_type = this.value.split('.')[1];
 
       if(!['pdf','xlsx'].includes(file_type.toLowerCase())){
           this.value = '';
+          const error_input = $("<p id='error_file_input' style='color:#a94442;' class='help-block help-block-error'>Ficherio must be pdf or xlsx</p>");
+          input_field.parentElement.append(error_input[0]);
           return;
       }
-
       input_type_field.value = file_type;
   };
 
   document.querySelectorAll('#document_new, document_edit').forEach(function(form){
     form.onsubmit = function() {
+        $("#error_file_input").remove();
+        if(input_field.value === ''){
+            const error_input = $("<p id='error_file_input' style='color:#a94442;' class='help-block help-block-error'>Ficheiro cannot be blank.</p>");
+            input_field.parentElement.append(error_input[0]);
+            return false;
+        }
         input_type_field.disabled = false;
     };
   });
